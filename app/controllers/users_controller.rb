3355c9a5e4f410bc63c57b91fec567690170ed3f
e155745 <e155745@ie.u-ryukyu.ac.jp>
2018-01-10 16:44:12 +0900

    Finish cha07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
+    # debugger
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user # = "/users/#{@user.id}"
+    else
+      render "new"
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
