e6ee2dbd46eabe9940e7fe657307d7ca4fac23f4
e155745 <e155745@ie.u-ryukyu.ac.jp>
2018-01-23 17:57:00 +0900

    finish cha13

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ before_action :admin_user,     only: :destroy
 
   def show
     @user = User.find(params[:id])
-    # debugger
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -54,15 +54,7 @@ before_action :admin_user,     only: :destroy
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-
-  def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-  end
-
+  
   def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
